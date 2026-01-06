e0bf5450613e56d9197b1464f49cce1d922cf1d4
Pooja Patel <poojaspatel1375@gmail.com>
2026-01-06 17:03:23 +0530

    feat: (PREV work )Add initial homepage with static assets and URL configuration. added and integrated properly

--- udaan_project/urls.py
+++ udaan_project/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("blood_request.urls")),
+    path("", TemplateView.as_view(template_name="home.html"), name="home"),
+    path("blood-request/", include("blood_request.urls")),
 ]
 
 # AUTOMATICALLY APPLY MIGRATIONS FOR IN-MEMORY DATABASE
